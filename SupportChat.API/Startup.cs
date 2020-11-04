1292538f4fcef4983da85849194b04a782c0ed12
Ignatella <ignatellasecond@gmail.com>
2020-11-04 01:42:11 +0100

    Add initial Docker support
    1) Add source code as a volume to allow react on changes.
    2) Set up remote debugger.
    3) Set up the place to store env variables.

--- SupportChat.API/Startup.cs
+++ SupportChat.API/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace SupportChat.API
 {
@@ -26,12 +25,7 @@ namespace SupportChat.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SupportChat.API", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +34,6 @@ namespace SupportChat.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SupportChat.API v1"));
             }
 
             app.UseHttpsRedirection();
